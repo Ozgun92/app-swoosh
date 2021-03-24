fb01ae79b23e17d7875040ca1011e7dc479bb578
Özgün Yildiz <ozgunyildiz@Ozguns-Air.local>
2021-03-24 17:23:09 +0100

    minor changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Özgün Yildiz on 24.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
