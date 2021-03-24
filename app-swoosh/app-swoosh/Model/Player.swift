fb021712c7a3d5a64253a4db5ed9a737ae610e2e
Özgün Yildiz <ozgunyildiz@Ozguns-Air.local>
2021-03-24 20:12:02 +0100

    App finished

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Özgün Yildiz on 24.03.21.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkill: String!
+}
